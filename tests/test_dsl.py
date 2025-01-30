0f807be85f4615200def2e51f5a11622a110e38e
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-01-30 15:13:16 +0100

    test: fix typo

--- tests/test_dsl.py
+++ tests/test_dsl.py
@@ -4,6 +4,6 @@ from karp_api_client.dsl import Equals
 def test_dsl(snapshot) -> None:
     q = Equals(field="baseform", value="agha") | Equals(field="baseform", value="agin")
 
-    actual = q.to_query_string()
+    actual = str(q)
 
     assert actual == snapshot

8d958e65e6e8ef4835508be1051c6d5bddd35a54
Kristoffer Andersson <kristoffer.andersson@gu.se>
2025-01-31 14:47:00 +0100

    refactor: rewrite examples for Python 3.9

--- examples/query-karp.py
+++ examples/query-karp.py
@@ -1,12 +1,8 @@
 """Example using this library in sync code."""
 
-from typing import Optional
-
-from returns.result import Failure, Success
-
 from karp_api_client import Client, dsl
 from karp_api_client.api import querying
-from karp_api_client.models.query_response import QueryResponse
+from karp_api_client.shared import Response
 
 
 def main() -> None:  # noqa: D103
@@ -19,23 +15,25 @@ def main() -> None:  # noqa: D103
             client=client,
             query_options=querying.QueryOptions(size=25, q=q),
         )
-        match response:
-            case Success(resp):
-                _print_table(resp.parsed)
-            case Failure(err):
-                print(f"Error occurred!\n{err}")  # noqa: T201
+        response.map(_print_table).lash(lambda err: print(f"Error occurred!\n{err}"))
+        # match response:
+        #     case Success(resp):
+        #         _print_table(resp.parsed)
+        #     case Failure(err):
+        #         print(f"Error occurred!\n{err}")
 
 
-def _print_table(response: Optional[QueryResponse]) -> None:
-    if response is None:
+def _print_table(response: Response) -> None:
+    print(f"{response=}")
+    if response.parsed is None:
         print("No response")  # noqa: T201
         return
     print(f"{'baseform':20s}{'resource':20s}entry")  # noqa: T201
-    for entry in response.hits:
+    for entry in response.parsed.hits:
         print(f"{entry.entry['baseform']:20s}{entry.resource:20s}{entry.to_dict()}")  # noqa: T201
 
     print("---")  # noqa: T201
-    print(f"showing {len(response.hits)} entries of {response.total} in total.")  # noqa: T201
+    print(f"showing {len(response.parsed.hits)} entries of {response.parsed.total} in total.")  # noqa: T201
 
 
 if __name__ == "__main__":
